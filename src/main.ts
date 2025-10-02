0f6d23a90200882c3ba0b47961efdd8fcfdbd18b
asoliman1 <elkhoulyahmedg@gmail.com>
2025-10-02 15:26:29 +0200

    Create UI/UX for currency dropdown component.

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,16 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
+import { CurrencyDropdownComponent } from './app/shared/componenets/currency-dropdown/currency-dropdown';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
+  standalone:true,
+  imports: [CurrencyDropdownComponent],
+  template: ` <app-currency-dropdown label=test></app-currency-dropdown>
   `,
 })
 export class App {
-  name = 'Angular';
 }
 
 bootstrapApplication(App, {
